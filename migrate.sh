5bd6a6835e9e0f4a83f125d9c151c19b0d438ef1
cknobloch <curt.knobloch@maxwellhealth.com>
2023-05-22 13:45:32 -0400

    Remove README info from migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -3,40 +3,12 @@
 #
 # Ex. usage: cd <schema_path> && ./migrate.sh mysql -h127.0.0.1 -P3307 -uclover -pclover
 #
-# This single "nameless" script is a super simple system for managing database migrations.
-# Basically, it reads a directory of sql files and runs any of them that haven't already run!
-# It tracks what files have run in a table in the target database/schema, similar to other tools.
-# Since the logic is so minimal, it's easy to port this script under any language/platform,
-# such as an ETL job.
-#
-# Getting started
-# 1. Put `migrate.sh` on your PATH or wherever you want to use it
-# 1. Copy an example directory
-# 1. Rename directories to match your database and schema
-# 1. Add schema directories (by copy+paste from example)
-# 1. Add revisions (copy+paste most recent under that schema)
-#
-# Hard conventions of the system
-# * All revisions for a schema should be kept under a single directory
-# * Filename identifies the revision (hopefully it also describes what it does, in a few words)
-# * Revisions are run in lexicographical order
-# * Filename should lead with an incrementing identifier.
-#   It doesn't matter exactly what it is as long as it's sequential (per the previous bullet),
-#   but incrementing digits like "00" -> "01" work well.
-# * There is no concept of downgrading only upgrading!
-#   If you have a risky migration and really want to prepare a downgrade ahead of time,
-#   do so in a code branch with a tentative ID.
-#   Or feel free to include downgrade steps using one of the conventions:
-#   * as a comment in the revision,
-#   * in another directory,
-#   * using a filename that doesn't get picked up by the pattern matching (ex. "_00_init.sql.revert" for "_00_init.sql")
-#
-# Implementation conventions
+# Notes:
 # * Filename can only contain word characters (no spaces)
 # * Directory name must match schema name exactly
 # * DB_CLIENT calls to get history, then run migration/update-history, aren't atomic
 #
-# Implementation TO DOs
+# TODO:
 # * Use of env vars (esp. for arguments) seems hacky, but I'm no bash expert
 #
 
